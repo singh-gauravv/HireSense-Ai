20760f6d9a605c10c282da702198851a0726f6ca
singhh-gauravv <gauravsunil2005@gmail.com>
2026-06-15 01:30:48 +0530

    Code move to backend folder

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -101,4 +101,17 @@ async function logoutUserController(req,res){
     })
 }
 
-module.exports = {registerUserController, loginUserController , logoutUserController}
+async function getMeController(req,res){
+    const user = await userModel.findById(req.user.id)
+
+    res.status(200).json({
+        message:"User details fetched successfully",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
+}
+
+module.exports = {registerUserController, loginUserController , logoutUserController , getMeController}
